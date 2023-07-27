24193b2fad1f0f18824ae90494ff7b54b4a692da
Jim Kardach <jim@kardach.clom>
2023-07-27 10:34:17 -0700

    Initial Commit

---
+++ Converter/ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConverterApp.swift
+//  Converter
+//
+//  Created by jim kardach on 7/27/23.
+//
+
+import SwiftUI
+
+@main
+struct ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
